7a0d5cc2c3f90d17c3b2017eb4f7160f9f7b1905
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-05-22 12:12:57 -0700

    created newsItem Array

--- YahooNews/ViewController.swift
+++ YahooNews/ViewController.swift
@@ -8,25 +8,81 @@
 
 import UIKit
 
+struct NewsItem {
+    var category: Categories
+    var headline: String
+}
+
+enum Categories {
+    case World, Americas, Europe, MiddleEast, Africa, Asia
+    
+    func string() -> String {
+        switch self {
+        case .World:
+            return "World"
+        case .Americas:
+            return "Americas"
+        case .Europe:
+            return "Europe"
+        case .MiddleEast:
+            return "Middle East"
+        case .Africa:
+            return "Africa"
+        case .Asia:
+            return "Asia"
+        }
+    }
+    
+    func color() -> UIColor {
+        switch self {
+        case .World:
+            return UIColor.red
+        case .Americas:
+            return UIColor.blue
+        case .Europe:
+            return UIColor.green
+        case .Africa:
+            return UIColor.orange
+        case .Asia:
+            return UIColor.purple
+        case .MiddleEast:
+            return UIColor.yellow
+        }
+    }
+}
+
 class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var newsItem = [NewsItem]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: true)
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 140
+    
+        let world = NewsItem.init(category: Categories.World, headline: "Climate change protests, divestments meet fossil fuels realities")
+        let americas = NewsItem.init(category: Categories.Americas, headline: "Officials: FBI is tracking 100 Americans who fought alongside IS in Syria")
+        let europe = NewsItem.init(category: Categories.Europe, headline: "Scotland's 'Yes' leader says independence vote is 'once in a lifetime'")
+        let middleEast = NewsItem.init(category: Categories.MiddleEast, headline: "Airstrikes boost Islamic State, FBI director warns more hostages possible")
+        let africa = NewsItem.init(category: Categories.Africa, headline: "Nigeria says 70 dead in building collapse; questions S. Africa victim claim")
+        let asia = NewsItem.init(category: Categories.Asia, headline: "Despite UN ruling, Japan seeks backing for whale hunting")
+        let world2 = NewsItem.init(category: Categories.World, headline: "South Africa in $40 billion deal for Russian nuclear reactors")
+        let europe2 = NewsItem.init(category: Categories.Europe, headline: "'One million babies' created by EU student exchanges")
+        
+        newsItem += [world, europe, middleEast, africa, asia, americas, world2, europe2]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return newsItem.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "yahooCell") as! YahooTableViewCell
-        cell.categoryLabel.text = "World"
-        cell.headlineLabel.text = "Climate change protests, divestments meet fossil fuels realities"
+        cell.categoryLabel.textColor = newsItem[indexPath.row].category.color()
+        cell.categoryLabel.text = newsItem[indexPath.row].category.string()
+        cell.headlineLabel.text = newsItem[indexPath.row].headline
         return cell
     }
     
